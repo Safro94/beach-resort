359f5206fba6aff7e094f82bb1c8b628195c4127
Matias Safranchik <Mati@MacBook-Pro-de-Matias.local>
2020-12-13 15:50:39 -0300

    add navbar

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -6,13 +6,18 @@ import NotFound from '../../pages/notFound';
 import Rooms from '../../pages/rooms';
 import Room from '../../pages/room';
 
+import Navbar from '../navbar';
+
+import { HOME, ROOMS, ROOM } from '../../utils/routes';
+
 const App: FC = () => {
   return (
     <>
+      <Navbar />
       <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/rooms' component={Rooms} />
-        <Route exact path='/rooms/:slug' component={Room} />
+        <Route exact path={HOME} component={Home} />
+        <Route exact path={ROOMS} component={Rooms} />
+        <Route exact path={ROOM} component={Room} />
         <Route component={NotFound} />
       </Switch>
     </>
---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,39 @@
+import React, { FC, useState } from 'react'
+import { Link } from 'react-router-dom'
+import { FaAlignRight } from 'react-icons/fa'
+
+import { HOME, ROOMS } from '../../utils/routes';
+
+import styles from './index.module.scss'
+import logo from '../../assets/images/logo.svg'
+
+const NavBar: FC = () => {
+  const [isOpen, setIsOpen] = useState<Boolean>(false);
+
+
+  const handleToogle = (): void => {
+    setIsOpen(prevState => !prevState);
+  }
+
+  return (
+    <nav className={styles.container}>
+      <div className={styles.center}>
+        <div className={styles.header}>
+          <Link to={HOME}>
+            <img src={logo} alt='Resort' />
+          </Link>
+          <button type='button' onClick={handleToogle} className={styles.button}>
+            <FaAlignRight className={styles.icon} />
+          </button>
+        </div>
+
+        <ul className={isOpen ? `${styles.links} ${styles.showNav}` : styles.links}>
+          <li><Link to={HOME}>Home </Link></li>
+          <li><Link to={ROOMS}>Rooms </Link></li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar
