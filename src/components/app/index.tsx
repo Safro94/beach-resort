825455660b31df6c74d5bc31d8c2054be94c4a00
Matias Safranchik <Mati@MacBook-Pro-de-Matias.local>
2020-12-13 14:18:33 -0300

    setup application

---
+++ src/components/app/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>hola</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
