57ca46d3bd5923df7f686914f3759c649e3f1e7d
Matias Safranchik <Mati@MacBook-Pro-de-Matias.local>
2020-12-27 13:14:55 -0300

    add room logic

--- src/pages/room/index.tsx
+++ src/pages/room/index.tsx
@@ -1,7 +1,44 @@
-import React, { FC } from 'react';
+import { FC, useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
+
+import Hero from '../../components/hero';
+import Banner from '../../components/banner';
+
+import NotFound from '../notFound';
+
+import { IRoom, useApplication } from '../../hooks/application';
+
+import { ROOMS } from '../../utils/routes';
+
+import defaultBg from '../../assets/images/room-1.jpeg';
+import styles from './index.module.scss';
 
 const Room: FC = () => {
-	return <div>Room</div>;
+	const { slug } = useParams<Record<string, string | undefined>>();
+	const { getRoom } = useApplication();
+	const [room, setRoom] = useState<IRoom>();
+
+	useEffect(() => {
+		const contextRoom = getRoom(slug || '');
+		setRoom(contextRoom);
+	}, [slug, getRoom]);
+
+
+	return (
+		<>
+			{
+				!room
+					? <NotFound />
+					: (
+						<Hero>
+							<Banner title={`${room.name} rooms`}>
+								<Link to={ROOMS} className={styles.button}>Back to rooms</Link>
+							</Banner>
+						</Hero>
+					)
+			}
+		</>
+	);
 };
 
 export default Room;
